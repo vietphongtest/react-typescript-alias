fc1912dd60eeeaedb3fc80ad04d70b1a7ed5a06f
vietphong <vietphong@192.168.1.3>
2023-05-20 15:41:53 +0700

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Form } from "components/Form";
+import { Layout } from "@layout/Header";
+
+export const Home = () => {
+  return (
+    <div>
+      <Form />
+      <Layout />
+    </div>
+  );
+};
